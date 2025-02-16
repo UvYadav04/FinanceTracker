504377bcb406da844eafa6381080dae40c8a7acf
Dinesh Yadav <dineshnirban01@gmail.com>
2025-02-16 21:55:04 +0530

    edited server function

--- app/server/route.ts
+++ app/server/route.ts
@@ -1,55 +1,51 @@
 import connectdb from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
-import expenses from '../../models/expense'
-export async function GET(req: NextRequest, res: NextResponse) {
+import expenses from '../../models/expense';
+
+export async function GET(req: NextRequest) {
     try {
-        await connectdb()
-        const alldata = await expenses.find({})
-        // console.log(alldata)
-        return NextResponse.json({ success: true, data: alldata })
+        await connectdb();
+        const alldata = await expenses.find({});
+        return NextResponse.json({ success: true, data: alldata });
     } catch (error) {
-        return NextResponse.json({ success: false })
+        return NextResponse.json({ success: false });
     }
 }
-export async function POST(req: NextRequest, res: NextResponse) {
+
+export async function POST(req: NextRequest) {
     try {
-        await connectdb()
-        const data = await req.json()
-        // console.log(data)
-        const newexpense = new expenses(data)
-        await newexpense.save()
-        // await expenses.insertMany(data)
-        const alldata = await expenses.find({})
-        // console.log(alldata)
-        return NextResponse.json({ success: true, data: alldata })
+        await connectdb();
+        const data = await req.json();
+        const newexpense = new expenses(data);
+        await newexpense.save();
+        const alldata = await expenses.find({});
+        return NextResponse.json({ success: true, data: alldata });
     } catch (error) {
-        return NextResponse.json({ success: false })
+        return NextResponse.json({ success: false });
     }
 }
-export async function PUT(req: NextRequest, res: NextResponse) {
+
+export async function PUT(req: NextRequest) {
     try {
-        await connectdb()
-        const data = await req.json()
-        console.log(data)
-        await expenses.findOneAndUpdate({ _id: data.id }, data)
-        const alldata = await expenses.find({})
-        // console.log(alldata)
-        return NextResponse.json({ success: true, data: alldata })
+        await connectdb();
+        const data = await req.json();
+        await expenses.findOneAndUpdate({ _id: data.id }, data);
+        const alldata = await expenses.find({});
+        return NextResponse.json({ success: true, data: alldata });
     } catch (error) {
-        return NextResponse.json({ success: false })
+        return NextResponse.json({ success: false });
     }
 }
-export async function DELETE(req: NextRequest, res: NextResponse) {
+
+export async function DELETE(req: NextRequest) {
     try {
-        await connectdb()
-        const data = await req.json()
-        // console.log("data : ", data)
-        await expenses.findOneAndDelete({ _id: data.id })
-        const alldata = await expenses.find({})
-        // console.log(alldata)
-        return NextResponse.json({ success: true, data: alldata })
+        await connectdb();
+        const data = await req.json();
+        await expenses.findOneAndDelete({ _id: data.id });
+        const alldata = await expenses.find({});
+        return NextResponse.json({ success: true, data: alldata });
     } catch (error) {
-        console.log(error)
-        return NextResponse.json({ success: false })
+        console.log(error);
+        return NextResponse.json({ success: false });
     }
 }
