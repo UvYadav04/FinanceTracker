85471ee46d5282c0688da5c0f53453bfe9528624
Dinesh Yadav <dineshnirban01@gmail.com>
2025-02-16 21:50:35 +0530

    edited an import

--- Components/Dashboard/Dialog.tsx
+++ Components/Dashboard/Dialog.tsx
@@ -14,7 +14,7 @@ import {
     SelectItem,
     SelectTrigger,
     SelectValue,
-} from "@/components/ui/select"
+} from "@../ui/select"
 
 import { Input } from "../ui/input"
 import { Label } from "../ui/label"
